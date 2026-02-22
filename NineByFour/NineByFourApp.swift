be1d3e1582753d802e27bdd8cbb5051aa78eb15c
Andre Wilkinson <andre_w3@yahoo.com>
2026-02-21 20:32:49 -0500

    Initial Commit

---
+++ NineByFour/NineByFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineByFourApp.swift
+//  NineByFour
+//
+//  Created by andre wilkinson on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct NineByFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
